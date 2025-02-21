d027d540a95bf3704f828b4c18a41583f31ff04a
KateWu6 <ceplienkova@gmail.com>
2025-02-21 18:26:03 +0300

    work with html

--- Project.go
+++ Project.go
@@ -2,37 +2,39 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 type Task struct {
-	task_name string
-	time      string
-	date      string
+	Task_name string
+	Time      string
+	Date      string
 }
 
 type User struct {
-	name        string
-	password    string
-	achievement uint16
-	energy      int16
-	lvl         uint16
-	tasks       []Task
+	Name        string
+	Password    string
+	Achievement uint16
+	Energy      int16
+	Lvl         uint16
+	Tasks       []Task
 }
 
 func (u User) getAllInfo() string {
 	return fmt.Sprintf("You name is: %s. Your password is %s and you "+
-		"have %d achievement", u.name, u.password, u.achievement)
+		"have %d achievement", u.Name, u.Password, u.Achievement)
 }
 
 func (u *User) setNewName(newName string) {
-	u.name = newName
+	u.Name = newName
 }
 
 func home_page(w http.ResponseWriter, r *http.Request) {
 	Bob := User{"Bob", "1234", 100, 10, 1, []Task{{"sport", "6.00", "01.02.25"}}}
-	Bob.setNewName("Alex")
-	fmt.Fprintf(w, Bob.getAllInfo())
+	//fmt.Fprintf(w, "<b>Main Text</b>")
+	tmpl, _ := template.ParseFiles("templates/home_page.html")
+	tmpl.Execute(w, Bob)
 }
 
 func contacts_page(w http.ResponseWriter, r *http.Request) {
@@ -42,7 +44,7 @@ func contacts_page(w http.ResponseWriter, r *http.Request) {
 func handlRequest() {
 	http.HandleFunc("/", home_page)
 	http.HandleFunc("/contacts/", contacts_page)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8000", nil)
 }
 
 func main() {
